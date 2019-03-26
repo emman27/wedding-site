659fc4d201677bc3473befa0efbcc83c4b9e33b4
Emmanuel Goh <emmanuel.goh.7@gmail.com>
2019-03-26 22:45:56 +0800

    feat(Add gcal link to landing page):
    Signed-off-by: Emmanuel Goh <emmanuel.goh.7@gmail.com>

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,4 +1,4 @@
-import { Card, Typography, withStyles } from "@material-ui/core";
+import { Card, Typography, withStyles, Icon } from "@material-ui/core";
 import Button, { ButtonProps } from "@material-ui/core/Button";
 import React from "react";
 import { Link } from "react-router-dom";
@@ -20,17 +20,31 @@ const styles = {
     padding: theme.spacing.unit,
     backgroundColor: "rgba(255,255,255,0.6)",
     borderRadius: 20,
-    maxWidth: 450,
+    maxWidth: 600,
     marginTop: 250,
+  },
+  buttons: {
+    margin: theme.spacing.unit,
   },
 };
 
 const page = ({ classes }: any) => (
-  <div style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}><Card className={classes.card}>
-    <Timer />
-    <LinkButton color="primary" variant="contained" size="large" component={Link as any} to="/rsvp">
-      <Typography variant="h5">RSVP <i className="fas fa-futbol" /></Typography>
-    </LinkButton>
-  </Card></div>);
+  <div style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
+    <Card className={classes.card}>
+      <Timer />
+      <LinkButton color="primary" variant="contained" size="large" component={Link as any} to="/rsvp">
+        <Typography variant="h5">RSVP <i className="fas fa-futbol" /></Typography>
+      </LinkButton>
+      <div className={classes.buttons}>
+        <Button
+          color="primary"
+          target="_blank"
+          href="https://calendar.google.com/event?action=TEMPLATE&amp;tmeid=MzRhNWRqOTcxaGd0aW1nbGI1NDZuYjIzajkgZXlnb2hsb2x6QG0&amp;tmsrc=eygohlolz%40gmail.com"
+          variant="contained">
+          <Icon>event</Icon> Google Calendar
+    </Button>
+      </div>
+    </Card>
+  </div>);
 
 export default withStyles(styles)(page);
